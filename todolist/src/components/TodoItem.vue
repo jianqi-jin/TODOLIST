1c2929d0160785074baba5218469fa31f9c55e9f
2359634711 <33452468+2359634711@users.noreply.github.com>
2018-05-09 18:45:07 +0800

    初始化
    项目创建

---
+++ todolist/src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="item" @click="handleClick">{{content}}</li>
+</template>
+
+<script>
+
+export default {
+  props: ['content', 'index'],
+  methods: {
+    handleClick () {
+      this.$emit('delete', this.index)
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .item{
+    color: green;
+  }
+</style>
